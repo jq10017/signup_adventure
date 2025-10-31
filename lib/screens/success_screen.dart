a31c62a369a1dd5e9df8671d55f9f45de4840f84
jq10017 <43541055+jq10017@users.noreply.github.com>
2025-10-31 10:58:01 -0400

    Adding screens

---
+++ lib/screens/success_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:confetti/confetti.dart';
+
+class SuccessScreen extends StatefulWidget {
+  // ... all the code for SuccessScreen and _SuccessScreenState ...
+}
